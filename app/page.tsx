2a2c3c6cde3817dba94f30953b5de59eef5bf5ec
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 18:43:26 -0300

    feat: import JSON for mapping new cars in ParentComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import { Table } from '@/components/Table'
+import booking from '../cars-booking.json'
+
+interface Car {
+  name: string
+  image: string
+  reservation: string
+  status: string
+  rating: number
+}
 
 export default function Home() {
-  const listNumber = [1, 2, 3, 4, 5]
+  const cars: Car[] = booking.car
 
   return (
     <div className="pt-[60px] md:flex md:h-screen md:w-full md:items-center md:justify-center md:pt-0">
       <Table.Root>
-        {listNumber.map((number) => (
-          <Table.Item key={number}>
-            <Table.Car text="Mini Cooper 2020" src="/car-mini-cooper.png" />
-            <Table.Reservation
-              startDate={new Date('2024-07-06')}
-              endDate={new Date('2024-07-12')}
-            />
-            <Table.Status status="available" />
-            <Table.Rating rating={5} />
+        {cars.map((car, index) => (
+          <Table.Item key={index}>
+            <Table.Car text={car.name} src={car.image} />
+            <Table.Reservation startDate={new Date(car.reservation)} />
+            <Table.Status status={car.status} />
+            <Table.Rating rating={car.rating} />
             <Table.Action />
           </Table.Item>
         ))}
