925f0544d2f235fd2d4beb468510adf9e8c23eff
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 18:41:42 -0300

    fix: correct badge bugs in TableStatus component

--- components/Table/TableStatus.tsx
+++ components/Table/TableStatus.tsx
@@ -10,18 +10,38 @@ export function TableStatus({ status }: TableStatusProps) {
   return (
     <div className="md:col-start-3 md:col-end-4 md:px-4">
       <div
-        className={`absolute hidden md:relative md:inline-block md:rounded-md md:bg-ui-green-100 md:px-[10px] md:pb-1
-          ${status === 'available' ? 'text-[#03543F]' : 'bg-[ui-red-100] text-ui-red-600'}`}
+        className={`absolute hidden md:relative md:inline-block md:rounded-md md:bg-ui-green-100 md:px-[10px]
+          ${status === 'available' ? 'text-[#03543F]' : 'text-ui-red-600 md:bg-ui-red-100'}`}
       >
         <span
-          className={'md:text-center md:text-xs md:font-medium md:capitalize'}
+          className={
+            'flex py-[2px] md:text-xs md:font-medium md:capitalize first-letter:md:text-center'
+          }
         >
           {status}
         </span>
       </div>
 
-      <div className="absolute left-[2px] top-[2px] rounded-full bg-ui-green-600 p-1 md:hidden">
-        <Image src="/check.svg" alt="status" width={16} height={16} />
+      <div
+        className={`absolute left-[2px] top-[2px] rounded-full p-1 
+        ${status === 'available' ? 'bg-ui-green-600' : 'bg-ui-red-600'} md:hidden`}
+      >
+        <Image
+          src="/check.svg"
+          alt="status"
+          width={0}
+          height={0}
+          sizes="16px"
+          className={`h-auto w-[16px] ${status === 'available' ? 'block' : 'hidden'}`}
+        />
+        <Image
+          src="/no-check.svg"
+          alt="status"
+          width={0}
+          height={0}
+          sizes="16px"
+          className={`h-auto w-[16px] ${status === 'unavailable' ? 'block' : 'hidden'}`}
+        />
       </div>
     </div>
   )
