196ec816f0ee9167375904ae2adadaf1c10911b6
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 15:09:18 -0300

    feat: implement TableItem component

---
+++ components/Table/TableItem.tsx
@@ -0,0 +1,14 @@
+interface TableItemProps {
+  children: React.ReactNode
+}
+
+export function TableItem({ children }: TableItemProps) {
+  return (
+    <div
+      className="h-[94px] mb-2 relative md:mb-0 md:min-h-0 md:w-full md:h-full md:grid grid-cols-2 
+      md:grid-cols-table md:items-center md:py-1 border-b border-[#E5E7EB]"
+    >
+      {children}
+    </div>
+  )
+}
