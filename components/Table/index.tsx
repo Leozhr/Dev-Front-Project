dd55d77b1d7e5e0dea0c1e13fa4bc92d65649069
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 15:14:36 -0300

    feat: implement index for Table components

---
+++ components/Table/index.tsx
@@ -0,0 +1,17 @@
+import { TableAction } from './TableAction'
+import { TableCar } from './TableCar'
+import { TableRoot } from './TableRoot'
+import { TableRating } from './TableRating'
+import { TableStatus } from './TableStatus'
+import { TableReservation } from './TableReservation'
+import { TableItem } from './TableItem'
+
+export const Table = {
+  Root: TableRoot,
+  Item: TableItem,
+  Action: TableAction,
+  Car: TableCar,
+  Rating: TableRating,
+  Status: TableStatus,
+  Reservation: TableReservation,
+}
