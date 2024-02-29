93bb705cda5bd39c7e6798e97b4d8d15f70b0cc8
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 15:11:21 -0300

    feat: implement TableReservation as a child component

---
+++ components/Table/TableReservation.tsx
@@ -0,0 +1,23 @@
+import { format } from 'date-fns'
+
+interface TableReservationProps {
+  startDate: Date
+  endDate: Date
+}
+
+export function TableReservation({
+  startDate,
+  endDate,
+}: TableReservationProps) {
+  function formatDate(date: Date) {
+    return format(date.setHours(24), 'MMM dd')
+  }
+
+  return (
+    <div className="mt-2 md:mt-0 md:px-4 md:col-start-2 md:col-end-3">
+      <h1 className="text-xs md:text-[#6B7280] md:text-sm">
+        {formatDate(startDate)} - {formatDate(endDate)}
+      </h1>
+    </div>
+  )
+}
