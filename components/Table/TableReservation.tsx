8d402154a79129358ecc0e0696cdcf3b462dff06
Leozhr <leonardo.leal202@gmail.com>
2024-02-29 16:03:31 -0300

    fix: Correct date bug in TableReservation component

--- components/Table/TableReservation.tsx
+++ components/Table/TableReservation.tsx
@@ -13,13 +13,16 @@ export function TableReservation({
     return format(date.setHours(24), 'MMM dd')
   }
 
+  const newStartDate = formatDate(startDate)
+  const newEndDate = formatDate(endDate)
+
   return (
     <div className="md:px-4 md:col-start-2 md:col-end-3">
       <span className="hidden md:block text-xs text-ui-gray-500 md:text-sm">
-        {formatDate(startDate)} - {formatDate(endDate)}
+        {newStartDate} - {newEndDate}
       </span>
       <span className="text-xs text-ui-gray-500 md:hidden md:absolute">
-        Next reservation: {formatDate(startDate)}
+        Next reservation: {newStartDate}
       </span>
     </div>
   )
